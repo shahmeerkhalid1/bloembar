f0299adbf72d8d4d7f6855b4f431db20102c6121
Shahmeer Khalid <shahmeer432@gmail.com>
2025-08-21 17:33:30 +0500

    remove resturant field from setting schema

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -510,7 +510,6 @@ export interface ApiSettSett extends Struct.SingleTypeSchema {
     updatedAt: Schema.Attribute.DateTime;
     updatedBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
-    zenchefResturantId: Schema.Attribute.String;
   };
 }
 
